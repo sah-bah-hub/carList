64c66bf3fc0cd2edeb246f5e8cf5ae2f81bab90a
sah-bah-hub <sashasbozhenko@gmail.com>
2024-06-11 23:21:27 +0300

    add path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-      head: {
-          title: 'CarList',
-      },
-  },
-  devtools: { enabled: true },
-  modules: ['@pinia/nuxt'],
-  build: { transpile: ['primevue'], },
-  css: [
-      '~/assets/scss/_main.scss',
-  ],
+    app: {
+        baseURL: '/carList/',
+        head: {
+            title: 'CarList',
+        },
+    },
+    devtools: { enabled: true },
+    modules: ['@pinia/nuxt'],
+    build: { transpile: ['primevue'], },
+    css: [
+        '~/assets/scss/_main.scss',
+    ],
 })
